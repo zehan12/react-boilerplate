e7eaa974b3d5f6ef123c8190d4162d5b529b800b
Zehan Khan <zehan9211@gmail.com>
2023-02-03 21:08:44 +0530

    app init with vite add test dep and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1 className='text-red-700 text-3xl font-bold'>Tailwind install</h1>
+      <h1 className='text-red-700 text-3xl font-bold'>Japanese Hiragana</h1>
     </div>
   )
 }
